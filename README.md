9866372e31dc6d43c03b9e96256d18544745f260
Harsh Shandilya <me@msfjarvis.dev>
2021-02-16 14:44:47 +0530

    build: upgrade to Kotlin 1.4.30
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # inlined
 
-Utility inline classes for Kotlin projects. Requires [Kotlin 1.4.30-M1] to enable use of [new features for inline classes].
+Utility inline classes for Kotlin projects. Requires [Kotlin 1.4.30] to enable use of [new features for inline classes].
 
 
-[kotlin 1.4.30-m1]: https://github.com/JetBrains/kotlin/releases/tag/v1.4.30-M1
+[kotlin 1.4.30]: https://github.com/JetBrains/kotlin/releases/tag/v1.4.30
 [new features for inline classes]: https://msfjarvis.dev/posts/improvements-to-inline-classes-in-kotlin-1-4-30/
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,9 @@
 buildscript {
   repositories {
-    mavenLocal()
-    jcenter()
-    google()
-    gradlePluginPortal()
-    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
-    maven { setUrl("https://kotlin.bintray.com/kotlinx") }
+    mavenCentral()
   }
 
   dependencies {
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30-M1")
-  }
-}
-
-allprojects {
-  repositories {
-    mavenLocal()
-    jcenter()
-    google()
-    gradlePluginPortal()
-    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
-    maven { setUrl("https://kotlin.bintray.com/kotlinx") }
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
   }
 }
