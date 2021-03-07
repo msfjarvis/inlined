2220120a2f766185003fce066a5c7612f0230a21
Harsh Shandilya <me@msfjarvis.dev>
2021-03-07 13:38:20 +0530

    build: switch to Kotlin 1.5.0-M1
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,25 @@
 buildscript {
   repositories {
-    mavenCentral()
+    mavenLocal()
+    jcenter()
+    google()
+    gradlePluginPortal()
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+    maven { setUrl("https://kotlin.bintray.com/kotlinx") }
   }
 
   dependencies {
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0-M1")
+  }
+}
+
+allprojects {
+  repositories {
+    mavenLocal()
+    jcenter()
+    google()
+    gradlePluginPortal()
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+    maven { setUrl("https://kotlin.bintray.com/kotlinx") }
   }
 }
